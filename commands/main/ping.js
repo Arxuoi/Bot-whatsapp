8c52a8ec3683fadaa92f1c474d6cc876e955ed41
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:32:04 +0700

    Update ping.js

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: "ping",
+  run: async ({ msg }) => {
+    const start = Date.now()
 
+    await msg.reply("Testing...")
+
+    const end = Date.now()
+
+    await msg.reply(`🏓 Pong\n${end - start} ms`)
+  }
+}
