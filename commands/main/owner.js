c32988134d5957c910454b1d7e7828f317508b5e
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:32:32 +0700

    Update owner.js

--- commands/main/owner.js
+++ commands/main/owner.js
@@ -1,1 +1,30 @@
+module.exports = {
+  name: "owner",
+  aliases: ["creator", "dev"],
+  run: async ({ sock, msg }) => {
 
+    const owner =
+      process.env.OWNER.replace(/[^0-9]/g, "")
+
+    await sock.sendMessage(
+      msg.from,
+      {
+        contacts: {
+          displayName: "Shen Owner",
+          contacts: [
+            {
+              displayName: "Shen Owner",
+              vcard:
+`BEGIN:VCARD
+VERSION:3.0
+FN:Shen Owner
+TEL;type=CELL;type=VOICE;waid=${owner}:${owner}
+END:VCARD`
+            }
+          ]
+        }
+      },
+      { quoted: msg.raw }
+    )
+  }
+}
