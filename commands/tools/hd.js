29f8ae22c7667b08189cc70b444fc8663ad61d11
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:30:49 +0700

    Update hd.js

--- commands/tools/hd.js
+++ commands/tools/hd.js
@@ -1,1 +1,33 @@
+const { hd } = require("../../lib/api")
 
+module.exports = {
+  name: "hd",
+  aliases: ["upscale", "upscaler"],
+  run: async ({ sock, msg, args }) => {
+    const imageUrl = args[0]
+    const set = args[1] || "2"
+
+    if (!imageUrl) {
+      return msg.reply(
+        "Masukin URL gambar dulu.\nContoh: !hd https://contoh.com/gambar.jpg"
+      )
+    }
+
+    const result = await hd(imageUrl, set)
+
+    const url =
+      result?.result ||
+      result?.url ||
+      result?.data?.url ||
+      result?.data?.result
+
+    if (!url) {
+      return msg.reply("Gagal ambil hasil HD dari API.")
+    }
+
+    await sock.sendMessage(msg.from, {
+      image: { url },
+      caption: "Done, gambar udah di-HD-in."
+    }, { quoted: msg.raw })
+  }
+}
