b234089999e02e6b4f8eac0c5c01ab36b84e8baa
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:30:34 +0700

    Update brat.js

--- commands/tools/brat.js
+++ commands/tools/brat.js
@@ -1,1 +1,18 @@
+const { brat } = require("../../lib/api")
 
+module.exports = {
+  name: "brat",
+  aliases: ["bratt"],
+  run: async ({ sock, msg, args }) => {
+    const text = args.join(" ")
+
+    if (!text) return msg.reply("Teksnya mana 😤\nContoh: !brat halo dunia")
+
+    const buffer = await brat(text)
+
+    await sock.sendMessage(msg.from, {
+      image: buffer,
+      caption: `Brat: ${text}`
+    }, { quoted: msg.raw })
+  }
+}
