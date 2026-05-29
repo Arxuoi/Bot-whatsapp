0702bede5a87007e638a95913acad384753df87f
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:32:18 +0700

    Update runtime.js

--- commands/main/runtime.js
+++ commands/main/runtime.js
@@ -1,1 +1,13 @@
+const { runtime } = require("../../lib/utils")
 
+module.exports = {
+  name: "runtime",
+  aliases: ["uptime"],
+  run: async ({ msg }) => {
+    const uptime = process.uptime()
+
+    await msg.reply(
+      `⏱ Runtime\n\n${runtime(uptime)}`
+    )
+  }
+}
