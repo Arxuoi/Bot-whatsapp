c1eed2db358696f6bb77e0a9d2cc8e8e695edd51
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-05-29 19:31:03 +0700

    Update sticker.js

--- commands/tools/sticker.js
+++ commands/tools/sticker.js
@@ -1,1 +1,28 @@
+const { Sticker, StickerTypes } = require("wa-sticker-formatter")
+const { downloadMediaMessage } = require("../../lib/downloader")
 
+module.exports = {
+  name: "sticker",
+  aliases: ["s", "stiker"],
+  run: async ({ sock, msg }) => {
+    try {
+      const buffer = await downloadMediaMessage(msg, false)
+
+      const sticker = new Sticker(buffer, {
+        pack: "Shen Bot",
+        author: "Shen",
+        type: StickerTypes.FULL,
+        quality: 80
+      })
+
+      const stickerBuffer = await sticker.toBuffer()
+
+      await sock.sendMessage(msg.from, {
+        sticker: stickerBuffer
+      }, { quoted: msg.raw })
+
+    } catch (e) {
+      await msg.reply("Reply/kirim gambar dulu pakai caption !sticker")
+    }
+  }
+}
